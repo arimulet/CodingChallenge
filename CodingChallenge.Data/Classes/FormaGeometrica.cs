6654cdb6609235a9c549b9e7cb1f3ad05fffdb8b
Ariel Mulet <mulet.ariel@gmail.com>
2022-03-10 18:59:38 -0300

    limpieza codigo

--- CodingChallenge.Data/Classes/FormaGeometrica.cs
+++ CodingChallenge.Data/Classes/FormaGeometrica.cs
@@ -9,41 +9,14 @@
 
 using System;
 using System.Collections.Generic;
-using System.Globalization;
 using System.Linq;
 using System.Text;
-using System.Threading;
 
 namespace CodingChallenge.Data.Classes
 {
     public abstract class FormaGeometrica
     {
-        #region Formas
-
-        public const int Cuadrado = 1;
-        public const int TrianguloEquilatero = 2;
-        public const int Circulo = 3;
-        public const int Trapecio = 4;
-
-        #endregion
-
-        #region Idiomas
-
-        public const int Castellano = 1;
-        public const int Ingles = 2;
-        
-
-        #endregion
-
         protected readonly decimal _lado;
-
-        public int Tipo { get; set; }
-
-        public FormaGeometrica(int tipo, decimal ancho)
-        {
-            Tipo = tipo;
-            _lado = ancho;
-        }
 
         public FormaGeometrica(decimal ancho)
         {
@@ -94,45 +67,7 @@ namespace CodingChallenge.Data.Classes
 
             return sb.ToString();
         }
-
-        
-        //private static string ObtenerLinea(int cantidad, decimal area, decimal perimetro, int tipo, int idioma)
-        //{
-        //    if (cantidad > 0)
-        //    {
-        //        if (idioma == Castellano)
-        //            return $"{cantidad} {TraducirForma(tipo, cantidad, idioma)} | Area {area:#.##} | Perimetro {perimetro:#.##} <br/>";
-
-        //        return $"{cantidad} {TraducirForma(tipo, cantidad, idioma)} | Area {area:#.##} | Perimeter {perimetro:#.##} <br/>";
-        //    }
-
-        //    return string.Empty;
-        //}
-
-        //private static string TraducirForma(int tipo, int cantidad, int idioma)
-        //{
-            
-        //    switch (tipo)
-        //    {
-        //        case Cuadrado:
-        //            if (idioma == Castellano) return cantidad == 1 ? "Cuadrado" : "Cuadrados";
-        //            else return cantidad == 1 ? "Square" : "Squares";
-        //        case Circulo:
-        //            if (idioma == Castellano) return cantidad == 1 ? "Círculo" : "Círculos";
-        //            else return cantidad == 1 ? "Circle" : "Circles";
-        //        case TrianguloEquilatero:
-        //            if (idioma == Castellano) return cantidad == 1 ? "Triángulo" : "Triángulos";
-        //            else return cantidad == 1 ? "Triangle" : "Triangles";
-        //    }
-
-        //    return string.Empty;
-        //}
-
-        public static string GetCuadradoTraduccion(Languages lang)
-        {
-            LanguageManager.SetLanguage(lang);
-            return Resource.Cuadrado;
-        }
+      
    
 
         public abstract decimal CalcularArea();
